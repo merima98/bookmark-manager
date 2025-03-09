1739b2bc592fa39be357e710f7a0eb9deefd635c
merima98 <merimaceranic98@gmail.com>
2025-03-09 12:07:55 +0100

    (feat): Adding Search bookmarks

--- components/logged-in-user.tsx
+++ components/logged-in-user.tsx
@@ -4,13 +4,21 @@ import NextImage from "next/image";
 import { Button } from "./ui/button";
 
 import bookmarkImage from "../public/assets/bookmark-image.png";
+import { Input } from "./ui/input";
 
 export default function LoggedInUser() {
   return (
-    <div className="flex p-4 gap-6">
-      TO DO
-      <div className="flex flex-col gap-16 ">Bookmark folders</div>
-      Preview of selected folder + option for adding new bookmark link
+    <div>
+      <div className="grid p-4 gap-6 grid-cols-[1fr_2fr]">
+        <span className="font-semibold">Bookmarks</span>
+        <Input placeholder="Search bookmarks" />
+      </div>
+      <div className="grid p-4 gap-6 grid-cols-[1fr_2fr]">
+        <div className="flex flex-col gap-16 ">Bookmark folders</div>
+        <div>
+          TO DO: Add selected folder and preview. Display filtered bookmarks.
+        </div>
+      </div>
     </div>
   );
 }
